f0ae95ee7e26f35cdc0ebe821f68f23dc335c812
Ali <mfateminia@gmail.com>
2020-03-19 15:26:33 +0100

    chore: hash

---
+++ src/utils/hash.ts
@@ -0,0 +1,16 @@
+import bcrypt from 'bcrypt';
+import { SHA256 } from 'sha2';
+
+export const hashString = async (string: string): Promise<string> => {
+    return bcrypt.hash(SHA256(string).toString('base64'), 10);
+};
+
+export const compareHash = async (
+    clearPassword: string,
+    hashedPassword: string
+  ) => {
+    return bcrypt.compare(
+      SHA256(clearPassword).toString('base64'),
+      hashedPassword
+    );
+};
