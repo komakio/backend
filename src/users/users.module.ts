66d130debf05220445f040d7ce5987ad1805d841
Ali <mfateminia@gmail.com>
2020-03-20 16:59:10 +0100

    chore: wip

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,9 +4,10 @@ import { UsersController } from './users.controller';
 import { UsersMongoService } from './services/users.mongo.service';
 import { MongoModule } from 'src/mongo/mongo.module';
 import { ConfigModule } from 'src/config/config.module';
+import { AuthService } from './auth/services/auth.service';
 
 @Module({
-    providers: [UsersService, UsersMongoService],
+    providers: [UsersService, UsersMongoService, AuthService],
     controllers: [UsersController],
     imports: [MongoModule, ConfigModule],
 })
