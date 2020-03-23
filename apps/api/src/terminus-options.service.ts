353c20c56bb8efdfb7104d0a01cb577bb7271a96
Théo Mathieu <tmathieu.github@fastmail.com>
2020-03-23 18:54:12 +0100

    fix: build (#18)

---
+++ apps/api/src/terminus-options.service.ts
@@ -0,0 +1,24 @@
+import {
+    TerminusEndpoint,
+    TerminusOptionsFactory,
+    // DNSHealthIndicator,
+    TerminusModuleOptions
+  } from '@nestjs/terminus';
+  import { Injectable } from '@nestjs/common';
+
+  @Injectable()
+  export class TerminusOptionsService implements TerminusOptionsFactory {
+    // constructor(
+    //   private readonly dns: DNSHealthIndicator,
+    // ) {}
+
+    public createTerminusOptions(): TerminusModuleOptions {
+      const healthEndpoint: TerminusEndpoint = {
+        url: '/healthcheck',
+        healthIndicators: [],
+      };
+      return {
+        endpoints: [healthEndpoint],
+      };
+    }
+  }
