ee307bc15253ed0c7eb88e7bdfb5e05d3a0df1b0
Ali <mfateminia@gmail.com>
2020-03-26 20:11:31 +0100

    chore: wip

--- libs/profiles/src/profiles.module.ts
+++ libs/profiles/src/profiles.module.ts
@@ -3,11 +3,12 @@ import { ProfilesService } from './profiles.service';
 import { ProfilesController } from './profiles.controller';
 import { ProfilesMongoService } from './services/profiles.mongo.service';
 import { MongoModule } from '@backend/mongo';
+import { ConfigModule } from '@backend/config';
 
 @Module({
   controllers: [ProfilesController],
   providers: [ProfilesService, ProfilesMongoService],
-  imports: [MongoModule],
+  imports: [MongoModule, ConfigModule],
   exports: [ProfilesService],
 })
 export class ProfilesModule {}
