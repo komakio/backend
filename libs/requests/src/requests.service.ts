bca299cbe535880d224497ce0d174f976652aa30
S.M. Ali Fateminia <mfateminia@gmail.com>
2020-03-27 13:00:51 +0100

    feat: needer receives notification upon approval (#51)

--- libs/requests/src/requests.service.ts
+++ libs/requests/src/requests.service.ts
@@ -4,12 +4,14 @@ import { HelpRequest } from './requests.model';
 import { ObjectID } from 'mongodb';
 import { ProfilesService } from '@backend/profiles';
 import { NotificationsService } from '@backend/notifications';
+import { UsersService } from '@backend/users';
 
 @Injectable()
 export class RequestsService {
   constructor(
     private requestsMongo: RequestsMongoService,
     private profiles: ProfilesService,
+    private users: UsersService,
     private notifications: NotificationsService
   ) {}
 
@@ -51,13 +53,36 @@ export class RequestsService {
 
   public async acceptOne(args: { id: ObjectID; acceptorProfileId: ObjectID }) {
     const profile = await this.profiles.findOneById(args.acceptorProfileId);
-    return this.requestsMongo.patchOneById({
+    await this.requestsMongo.patchOneById({
       id: new ObjectID(args.id),
       data: {
         status: 'accepted',
         acceptorProfileId: new ObjectID(args.acceptorProfileId),
         profileIds: [],
         acceptorShortName: profile.firstName,
+      },
+    });
+    const request = await this.findOneById(new ObjectID(args.id));
+    const requesterProfile = await this.profiles.findOneById(
+      new ObjectID(request.requesterProfileId)
+    );
+    const requesterUser = await this.users.findOneById(
+      new ObjectID(requesterProfile.userId)
+    );
+    const registrationToken =
+      requesterUser.uuidRegTokenPair &&
+      Object.values(requesterUser.uuidRegTokenPair)?.[0];
+
+    if (!registrationToken) {
+      return;
+    }
+
+    await this.notifications.send({
+      registrationTokens: [registrationToken],
+      message: {
+        title: 'KOMAK',
+        body: 'Someone accepted you request.',
+        icon: '',
       },
     });
   }
