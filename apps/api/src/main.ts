49fbdf9f6b335e07c21018949b53cc902251fc4c
S.M. Ali Fateminia <mfateminia@gmail.com>
2020-03-29 16:22:56 +0200

    fix: swagger (#65)

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
     .setTitle('Komak')
     .setDescription('The Komak API description')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
--- utils/decorators/auth.decorator.ts
+++ utils/decorators/auth.decorator.ts
@@ -1,14 +1,24 @@
 import { SetMetadata, applyDecorators } from '@nestjs/common';
+import { ApiBearerAuth, ApiResponse } from '@nestjs/swagger';
 
 export type Role = 'admin' | 'super-admin';
 
 export function Auth(roles?: Role | Role[], desactivate = false) {
-    if (desactivate) {
-        return applyDecorators();
-    }
-    const decorators: (ClassDecorator | MethodDecorator)[] = [SetMetadata('isLoggedIn', [])];
-    if (roles) {
-        decorators.push(SetMetadata('roles', Array.isArray(roles) ? roles : [roles]));
-    }
-    return applyDecorators(...decorators);
+  if (desactivate) {
+    return applyDecorators();
+  }
+  const decorators: (ClassDecorator | MethodDecorator)[] = [
+    SetMetadata('isLoggedIn', []),
+    ApiBearerAuth(),
+    ApiResponse({
+      status: 403,
+      description: 'Forbidden.',
+    }),
+  ];
+  if (roles) {
+    decorators.push(
+      SetMetadata('roles', Array.isArray(roles) ? roles : [roles])
+    );
+  }
+  return applyDecorators(...decorators);
 }
