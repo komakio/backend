427441b2ce2932fef5176d4b5c6bc7240caed4e5
Ali <mfateminia@gmail.com>
2020-03-29 17:33:12 +0200

    fix: distance formula

--- utils/distance.ts
+++ utils/distance.ts
@@ -10,10 +10,11 @@ export const getDistance = (args: {
   const dLat = deg2rad(args.from[0] - args.to[0]);
   const dLon = deg2rad(args.from[1] - args.to[1]);
   const a =
-    Math.sqrt(Math.sin(dLat / 2)) +
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
     Math.cos(deg2rad(args.from[0])) *
       Math.cos(deg2rad(args.to[0])) *
-      Math.sqrt(Math.sin(dLon / 2));
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
   const angularDistance = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   return Math.floor(R * angularDistance); // Distance in m
 };
