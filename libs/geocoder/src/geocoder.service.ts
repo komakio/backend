3a5f6fd8d8a4b790a1074e7fa9a4c98e26b7cbb3
Ali <mfateminia@gmail.com>
2020-04-03 17:18:40 +0200

    feat: added countryAcronym and city Closes #92

--- libs/geocoder/src/geocoder.service.ts
+++ libs/geocoder/src/geocoder.service.ts
@@ -71,6 +71,8 @@ export class GeocoderService {
       latitude: feature.geometry.coordinates[1],
       label: feature.properties.label,
       layer: feature.properties.layer,
+      countryAcronym: feature.properties.country_a,
+      city: feature.properties.locality,
     }));
   }
 }
