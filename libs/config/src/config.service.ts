824b5c751c25fb68d64335e1bfdb14662f3b66ce
Ali <mfateminia@gmail.com>
2020-04-04 20:45:48 +0200

    feat: email service

--- libs/config/src/config.service.ts
+++ libs/config/src/config.service.ts
@@ -52,4 +52,10 @@ export class ConfigService {
       'du:<9tv:B{P^X/s~Lv4+MsT7.>%w%!A$zU%_C~Pj3[,]+~c2LB;;fLBE5E@NKFg',
     accessTokenExpiration: 60 * 60 * 24 * 365,
   };
+
+  public mailgun = {
+    apiKey: process.env.EMAIN_API_KEY,
+    domain: process.env.EMAIN_DOMAIN,
+    from: process.env.EMAIN_ADDRESS,
+  };
 }
