d55f753f4f529e28361c9c648b91f16b634f78d9
Ali <mfateminia@gmail.com>
2020-04-09 14:32:10 +0200

    fix: delayed exchange init

--- apps/consumers/src/main.ts
+++ apps/consumers/src/main.ts
@@ -69,6 +69,6 @@ async function bootstrap() {
     .filter(q => q.withDelayedExchange)
     .map(q => rabbitmq.initDelayedExchange(q.queueName));
 
-  await Promise.all(queues);
+  await Promise.all(queues.map(q => bootstrapQueue(q)));
 }
 bootstrap();
