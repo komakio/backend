e87358d1d433c85452c12aee90be52a708779bbb
S.M. Ali Fateminia <mfateminia@gmail.com>
2020-04-10 08:55:08 +0200

    feat: max rate limit added (#103)

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,20 +1,41 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { RabbitMQService } from '@backend/rabbitmq';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ExceptionsService } from '@backend/exceptions';
 import { ConfigService } from '@backend/config';
+import RedisStore from 'rate-limit-redis';
+import RateLimit from 'express-rate-limit';
+import { RedisService } from '@backend/redis';
+import { Request } from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.set('trust proxy');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   process.on('uncaughtException', (err: Error) => {
     app.get(ExceptionsService).report(err);
   });
 
+  const redis = app.get(RedisService);
   await app.get(RabbitMQService).connect();
+  await redis.waitReady();
+
+  app.use(
+    RateLimit({
+      store: new RedisStore({
+        client: redis.db,
+        prefix: `${redis.prefix}:ratelimit:`,
+      }),
+      max: 100,
+      keyGenerator: (req: Request) => req.headers.authorization || req.ip,
+    })
+  );
+
   const options = new DocumentBuilder()
     .setTitle('Komak')
     .setDescription('The Komak API description')
