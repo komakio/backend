72bc883c435342aefd52c5ccfee3ce030c2e041e
Ali <mfateminia@gmail.com>
2020-04-11 16:09:21 +0200

    chore: wip

--- utils/templates.ts
+++ utils/templates.ts
@@ -12,13 +12,13 @@ export const getRequestAcceptedTemplate = (args: {
             <mj-section>
                 <mj-column>
                     <mj-image src="https://images.prismic.io/komak/96514f1a-2c09-42f5-b1b1-9d06bd7bf6e1_%28Put+Here%29+Cover+%26+Profile+photo+%281%29.png?auto=compress,format"></mj-image>
-                    <mj-text font-size="16px" font-family="Sen">Hey ${args.requesterName}</mj-text>
-                    <mj-text font-size="16px" font-family="Sen" line-height="1.5">${args.acceptorName} has agreed to help you out. Give them a call to let them know what you need help with. You can find their contact details below.</mj-text>
-                    <mj-text font-size="16px" font-family="Sen"  line-height="0.5">Name: ${args.requesterName}</mj-text>
-                    <mj-text font-size="16px" font-family="Sen" line-height="0.5">Phone number: ${args.acceptorPhone}</mj-text>
-                    <mj-text font-size="16px" font-family="Sen"></mj-text>
-                    <mj-text font-size="16px" font-family="Sen" line-height="0.3">Have a great day,</mj-text>
-                    <mj-text font-size="16px" font-family="Sen" line-height="0.1">The Komak Team</mj-text>
+                    <mj-text font-size="16px" font-family="Sen" font-weight=400>Hey ${args.requesterName}</mj-text>
+                    <mj-text font-size="16px" font-family="Sen" line-height="1.5" font-weight=400>${args.acceptorName} has agreed to help you out. Give them a call to let them know what you need help with. You can find their contact details below.</mj-text>
+                    <mj-text font-size="16px" font-family="Sen"  line-height="0.5" font-weight=400>Name: ${args.requesterName}</mj-text>
+                    <mj-text font-size="16px" font-family="Sen" line-height="0.5" font-weight=400>Phone number: ${args.acceptorPhone}</mj-text>
+                    <mj-text font-size="16px" font-family="Sen" font-weight=400></mj-text>
+                    <mj-text font-size="16px" font-family="Sen" line-height="0.3" font-weight=400>Have a great day,</mj-text>
+                    <mj-text font-size="16px" font-family="Sen" line-height="0.1" font-weight=400>The Komak Team</mj-text>
                 </mj-column>
             </mj-section>
         </mj-body>
